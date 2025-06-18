d7c91a0c14f930872e71a0eb730234367b04bf39
tal-gaffan <tal.gaffan@gmail.com>
2025-06-18 12:12:53 +0300

    Changes:

--- fold/fold.cpp
+++ fold/fold.cpp
@@ -9,22 +9,26 @@ using std::cout;
 using std::endl;
 using std::setw;
 
-constexpr int START = 1;
-constexpr int END = 11;
+constexpr int FIRST_MULTIPLE = 1;
+constexpr int LAST_MULTIPLE = 11;
 constexpr int SUCCESS_CODE = 0;
 constexpr int SPACES_PER_PROD = 3;
 constexpr int BARRIER_THICKNESS = 1;
 
 int main() {
-	string lineBarrier = string(HORIZONTAL_BARRIER, (END - START + 1) * (SPACES_PER_PROD + BARRIER_THICKNESS) + 1);
-	cout << lineBarrier << endl;
-	for (int i = START; i <= END; i++) {
+	int line_length = ((LAST_MULTIPLE - FIRST_MULTIPLE + 1) * (SPACES_PER_PROD + BARRIER_THICKNESS)) + 1;
+	string line_barrier = string(line_length, HORIZONTAL_BARRIER);
+
+	cout << line_barrier << endl;
+	for (int i = FIRST_MULTIPLE; i <= LAST_MULTIPLE; i++) {
 		cout << VERTICAL_BARRIER;
-		for (int j = START; j <= END; j++) {
+
+		for (int j = FIRST_MULTIPLE; j <= LAST_MULTIPLE; j++) {
 			cout << setw(SPACES_PER_PROD) << i * j << VERTICAL_BARRIER;
 		}
+
 		cout << endl;
-		cout << lineBarrier << endl;
+		cout << line_barrier << endl;
 	}
 	return SUCCESS_CODE;
 }

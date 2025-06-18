f1e419019b82ebc7879cb751febc8a3ba5759824
tal-gaffan <tal.gaffan@gmail.com>
2025-06-18 13:00:25 +0300

    Code format correctly

--- fold/fold.cpp
+++ fold/fold.cpp
@@ -9,26 +9,32 @@ using std::cout;
 using std::endl;
 using std::setw;
 
-constexpr int FIRST_MULTIPLE = 1;
-constexpr int LAST_MULTIPLE = 11;
+constexpr unsigned int FIRST_MULTIPLE = 1;
+constexpr unsigned int LAST_MULTIPLE = 11;
 constexpr int SUCCESS_CODE = 0;
-constexpr int SPACES_PER_PROD = 3;
-constexpr int BARRIER_THICKNESS = 1;
+constexpr unsigned int SPACES_PER_PROD = 3;
+constexpr unsigned int BARRIER_THICKNESS = 1;
+constexpr unsigned int LINE_LENGTH = (LAST_MULTIPLE - FIRST_MULTIPLE + 1) * (SPACES_PER_PROD + BARRIER_THICKNESS) + 1;
+
+const string LINE_BARRIER = string(LINE_LENGTH, HORIZONTAL_BARRIER);
+
+void print_line(int i) {
+	cout << VERTICAL_BARRIER;
+
+	for (int j = FIRST_MULTIPLE; j <= LAST_MULTIPLE; j++) {
+		cout << setw(SPACES_PER_PROD) << i * j << VERTICAL_BARRIER;
+	}
+
+	cout << endl;
+}
 
 int main() {
-	int line_length = ((LAST_MULTIPLE - FIRST_MULTIPLE + 1) * (SPACES_PER_PROD + BARRIER_THICKNESS)) + 1;
-	string line_barrier = string(line_length, HORIZONTAL_BARRIER);
+	cout << LINE_BARRIER << endl;
 
-	cout << line_barrier << endl;
 	for (int i = FIRST_MULTIPLE; i <= LAST_MULTIPLE; i++) {
-		cout << VERTICAL_BARRIER;
-
-		for (int j = FIRST_MULTIPLE; j <= LAST_MULTIPLE; j++) {
-			cout << setw(SPACES_PER_PROD) << i * j << VERTICAL_BARRIER;
-		}
-
-		cout << endl;
-		cout << line_barrier << endl;
+		print_line(i);
+		cout << LINE_BARRIER << endl;
 	}
+
 	return SUCCESS_CODE;
 }

8805f37a9f541696cbaafb3c87ae1137c2dcd27f
tal-gaffan <tal.gaffan@gmail.com>
2025-06-18 17:08:33 +0300

    Moved exception classes into the calculator.cpp file

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,21 +1,60 @@
 #include "calculator.h"
 
-#include "DivisionByZeroException.h"
-#include "InvaildOperatorException.h"
+// Invalid Operation Exception class and implementation
+class InvalidOperatorException {
+public:
+	InvalidOperatorException();
 
-// Implement all calculator class functions
+	string errorMessage();
+
+private:
+	const string m_errorMsg;
+};
+
+// Constructor implementation
+InvalidOperatorException::InvalidOperatorException() : m_errorMsg(INVALID_OPERATOR) {}
+
+// errorMessage implementation
+string InvalidOperatorException::errorMessage() {
+	return this->m_errorMsg;
+}
+
+// Division By Zero Exception Class and Implementation
+class DivisionByZeroException {
+public:
+	DivisionByZeroException();
+
+	string errorMessage();
+
+private:
+	const string m_errorMsg;
+};
+
+// Constructor implementation
+DivisionByZeroException::DivisionByZeroException() : m_errorMsg(DIVISION_BY_ZERO) {}
+
+// errorMessage implementation
+string DivisionByZeroException::errorMessage() {
+	return this->m_errorMsg;
+}
+
+// Implementation of Calculator class
+// returns x + y
 double Calculator::add(double x, double y) {
 	return x + y;
 }
 
+// returns x - y
 double Calculator::subtract(double x, double y) {
 	return x - y;
 }
 
+// returns x * y
 double Calculator::multiply(double x, double y) {
 	return x * y;
 }
 
+// returns x / y
 double Calculator::divide(double x, double y) {
 	if (y == 0) {
 		throw DivisionByZeroException();
@@ -23,15 +62,16 @@ double Calculator::divide(double x, double y) {
 	return x / y;
 }
 
+// returns x op y where op is one of +, -, *, /
 double Calculator::calculate(double x, char op, double y) {
 	switch (op) {
-	case '+':
+	case ADD:
 		return add(x, y);
-	case '-':
+	case SUBTRACT:
 		return subtract(x, y);
-	case '*':
+	case MULTIPLY:
 		return multiply(x, y);
-	case '/':
+	case DIVIDE:
 		return divide(x, y);
 	default:
 		throw InvalidOperatorException();
@@ -41,7 +81,7 @@ double Calculator::calculate(double x, char op, double y) {
 int main() {
 	Calculator calc;
 	try {
-		std::cout << calc.calculate(2, '$', 0);
+		std::cout << calc.calculate(21, '%', 0);
 	}
 	catch (DivisionByZeroException err) {
 		std::cout << err.errorMessage() << std::endl;
--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,3 +1,15 @@
+#include <iostream>
+
+#define INVALID_OPERATOR "Invalid Operator!"
+#define DIVISION_BY_ZERO "Divison by Zero!"
+
+using std::string;
+
+constexpr char ADD = '+';
+constexpr char SUBTRACT = '-';
+constexpr char MULTIPLY = '*';
+constexpr char DIVIDE = '/';
+
 class Calculator {
 public:
 	double add(double x, double y);
